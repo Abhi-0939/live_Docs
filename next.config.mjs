83b93ddd27c66c08c3d721c89df7787f2a834cc7
Abhiraj Gaikwad <154492869+Abhi-0939@users.noreply.github.com>
2024-08-03 12:49:02 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript : {
-     ignoreBuildErrors: true;
+     ignoreBuildErrors: true,
   },
     images: {
         remotePatterns: [{ protocol: 'https', hostname: 'img.clerk.com' }]
